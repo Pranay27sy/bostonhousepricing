d33d2146eeb19bc22ad59dcfa2774edd12080906
Pranay Yadav <pranay99yadav@gmail.com>
2023-08-28 13:39:24 +0530

    commit2
    created scaling.pkl, app.py & home page template.

---
+++ app.py
@@ -0,0 +1,27 @@
+import pickle
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+# Load the model
+with open('lr_model.pkl','rb') as file :
+    lr_model = pickle.load(file)
+
+with open('scaling.pkl','rb') as file :
+    scaler = pickle.load(file)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api() :
+    data = request.json['data']
+    print(data)
+    scaled_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    output = lr_model.predict(scaled_data)[0]
+    return jsonify(output)
+
+if __name__ == '__main__' :
+    app.run(debug=True)
